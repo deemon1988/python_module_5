8e6caba9f97bba01db87100b0d5408808219a228
deemon1988 <dmn72835@gmail.com>
2024-11-11 12:44:13 +0300

    Задача "Магические здания"

--- classandobject.py
+++ classandobject.py
@@ -1,6 +1,23 @@
 class Human:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
+    def say_info(self):
+        print(f'Привет, меня завут {self.name}, мне {self.age} лет')
+    def birthday(self):
+        self.age += 1
+        print(f'У меня день рождения, мне теперь {self.age}')
+    def __del__(self):
+        print(f'{self.name} ушел')
+    def __len__(self):
+        return self.age
 
-dim = Human('Дмитрий')
-print(dim.name)
+dim = Human('Дмитрий', 35)
+den = Human('Денис', 35)
+dim.say_info()
+den.say_info()
+
+del dim
+den.birthday()
+input()
+print(len(den))
