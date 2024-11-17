59de35f5faa2b67fb4a2a10cd80a39325a30d4ca
deemon1988 <dmn72835@gmail.com>
2024-11-17 15:09:23 +0300

    Задание "Свой YouTube" current user

--- module5hard.py
+++ module5hard.py
@@ -1,11 +1,11 @@
 #  Дополнительное практическое задание по модулю: "Классы и объекты."
-from email.charset import add_alias
-from operator import contains
+
+import time
 
 
 class UrTube:
         def __init__(self, users:list=None, videos:list=None, current_user=None):
-            self.users = users
+            self.users = []
             self.videos = videos
             self.current_user = current_user
 
@@ -20,15 +20,13 @@ class UrTube:
                 if len(add_videos) > 0:
                     self.videos += video
                     print("Добавленные видео:", [v.title for v in add_videos])
-
             else:
                 self.videos = video
                 print("Добавленные видео:", [v.title for v in add_videos])
 
 
-
         def log_in(self, nickname, password):
-            if nickname in self.users :
+            if self.users != None and nickname in self.users :
                 for i in self.users:
                     if i.nickname == nickname:
                         if i.password == hash(str(password)):
@@ -40,15 +38,39 @@ class UrTube:
                             break
                     else:
                         continue
+            else:
+                print("Зарегистрируйтесь")
 
         def logout(self):
             self.current_user = None
 
         def register(self, nickname: str, password: int, age: int):
-            if nickname in self.users:
-                print(f"Пользователь {nickname} уже существует")
-            else:
-                self.users.append(User(nickname,password,age))
+             if nickname in self.users :
+                    print(f"Пользователь {nickname} уже существует")
+             else:
+                 self.users.append(User(nickname,password,age))
+
+        def get_videos(self, search_word):
+            find_videos = []
+            for i in ur.videos:
+                if search_word.upper() in i.title.upper():
+                    find_videos.append(i.title)
+            return find_videos
+
+        def watch_video(self, film_title):
+           watch = [Video for Video in ur.videos if Video.title == film_title]
+           if watch != None and self.current_user != None:
+               film = watch.pop()
+               if int(self.current_user.age) >= 18 and film.adult_mode:
+                   while film.time_now <= film.duration:
+                       print(film.time_now)
+                       time.sleep(1)
+                       film.time_now += 1
+                   print("Конец видео")
+               else:
+                   print("Вам нет 18 лет, пожалуйста покиньте страницу")
+           else:
+               print("Войдите в аккаунт, чтобы смотреть видео")
 
 class Video:
     def __init__(self, title, duration, time_now=0, adult_mode=False):
@@ -56,6 +78,9 @@ class Video:
         self.duration = duration
         self.time_now = time_now
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return f"Название: {self.title}, Длительность: {self.duration}"
 
 class User:
 
@@ -67,7 +92,8 @@ class User:
         def __eq__(self, other):
             return self.nickname == other
 
-
+        def __str__(self):
+            return f"{self.nickname}"
 #user = User(nickname:=input("Имя пользователя: "), password := input("Пароль: "), age := input("Возвраст: "))
 user = User('dim', 123, 23)
 user2 = User('den', 421, 24)
@@ -78,21 +104,7 @@ film = Video('comedy', 100)
 clip = Video('musical', 20, adult_mode=True)
 doc = Video('documental', 50)
 
-urban = UrTube([user,user2,user3], [film,clip,doc])
 
-urban.log_in('max',231)
-print(urban.current_user)
-
-urban.add(film, clip)
-urban.add(clip)
-
-for i in urban.videos:
-    print(i.title)
-
-urban.register('hello',123, 23)
-
-for i in urban.users:
-    print(i.nickname)
 
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
@@ -102,3 +114,15 @@ ur.add(v1, v2)
 ur.add(clip, film)
 ur.add(clip, film)
 print("Все видео:",[v.title for v in ur.videos])
+
+ur.register('dim',123, 23)
+ur.register('den', 421, 24)
+
+
+print(ur.get_videos('Прог'))
+
+ur.log_in("dim", 123)
+
+
+ur.watch_video("musical")
+
